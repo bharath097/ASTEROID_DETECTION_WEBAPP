fb721342f4f5ee04f1f3e16621d5018ab6331593
Bharath Chandra N <93869875+bharath097@users.noreply.github.com>
2024-05-14 23:01:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -44,4 +44,4 @@ def predict():
         return render_template('index.html', pred = f'HAZARDOUS TO EARTH')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, host="0.0.0.0")
